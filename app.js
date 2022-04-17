8b6b8afa81881dfc3dd95623b6793c4daa85af53
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-04-17 11:23:51 +0530

    removed redundant search in js

--- app.js
+++ app.js
@@ -21,17 +21,3 @@ for (i = 1 ; i < 100 ; i++)
     pagenr++;
 }
 
-function search_char() {
-    let input = document.getElementById('search').value;
-    input = input.toLowerCase();
-    let x = document.querySelectorAll('div > h2');
-
-    for (i = 0 ; i < x.length ; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="block";  
-    }
-    }
-}
